444cba48fc77859f3d3225b574b93cb7a8633974
stanley <stanleyjo41@gmail.com>
2024-10-29 21:07:24 +0700

    add carousel feature to header image

--- assets/js/custom-scripts.js
+++ assets/js/custom-scripts.js
@@ -15,3 +15,29 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+const headerImages = [
+    'assets/img/header-bg2.jpg',
+    'assets/img/header-bg3.jpg',
+    'assets/img/header_bg.jpg'
+]
+
+// Interval for carousel (5 seconds)
+const carouselInterval = 5000;
+let currentImageIndex = 0;
+
+function changeHeaderBackground() {
+    const headerWrap = document.getElementById('headerwrap');
+
+    // Apply the new background image with all required styles
+    headerWrap.style.backgroundImage = `url(${headerImages[currentImageIndex]})`;
+    headerWrap.style.backgroundSize = 'cover';
+    headerWrap.style.backgroundPosition = 'center center';
+    headerWrap.style.backgroundRepeat = 'no-repeat';
+
+    // Update to the next image in the array
+    currentImageIndex = (currentImageIndex + 1) % headerImages.length;
+}
+
+// Initialize the carousel
+setInterval(changeHeaderBackground, carouselInterval);
